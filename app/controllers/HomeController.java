81c39efa6450555374aee20e91910ab3627c346c
efegüre <efe__gure@hotmail.com>
2016-08-18 00:48:14 +0300

    quick fix

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -116,14 +116,15 @@ public class HomeController extends Controller {
 		for(int i=0;i<userlist.size();i++){
 			User user = userlist.get(i);
 			verification = Password.checkPassword(user.email.toString(),hashed);
-			if(verification==false){
+			if(verification){
 				name = user.name.toString();
 				User us=User.find.byId(user.email);
 				us.register();
 				us.save();
+				break;
 			}
 		}
-		if(verification==false){
+		if(verification){
 			return ok(views.html.validate.render(name));
 		}
 		else{
